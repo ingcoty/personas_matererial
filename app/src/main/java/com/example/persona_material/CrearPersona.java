0f41e35080770b98ec6b38be8bec3f843973e9a3
Carlos Cabas <ing.carloscabas@gmail.com>
2020-11-20 21:26:32 -0500

    activity crear personas terminada

---
+++ app/src/main/java/com/example/persona_material/CrearPersona.java
@@ -0,0 +1,14 @@
+package com.example.persona_material;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearPersona extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_persona);
+    }
+}
